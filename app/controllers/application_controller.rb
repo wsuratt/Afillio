cf00c9177f2d6594cc61f2b74c32732aa5e9e061
wsuratt <wsuratt@comcast.net>
2021-05-01 05:10:06 +0000

    index search

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   
-  before_action :set_global_variables, if: :user_signed_in?
+  before_action :set_global_variables
   def set_global_variables
     @ransack_products = Product.ransack(params[:products_search], search_key: :products_search) #navbar search
   end
