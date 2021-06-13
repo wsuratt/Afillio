291920a8a5534f0a40e6149f537f12114deee094
wsuratt <wsuratt@comcast.net>
2021-06-13 04:36:58 +0000

    my_sales path

--- app/controllers/stripe_controller.rb
+++ app/controllers/stripe_controller.rb
@@ -9,13 +9,13 @@ class StripeController < ApplicationController
     )
 
     if response.parsed_response.key?("error")
-      redirect_to user_settings_path(user),
+      redirect_to my_sales_orders_path,
         notice: response.parsed_response["error_description"]
     else
       stripe_user_id = response.parsed_response["stripe_user_id"]
       current_user.update_attribute(:stripe_user_id, stripe_user_id)
 
-      redirect_to user_settings_path(user),
+      redirect_to my_sales_orders_path,
         notice: 'User successfully connected with Stripe!'
     end
   end
