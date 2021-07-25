7d0762e2767298e605efdcedbf7977370c2bb670
wsuratt <wsuratt@comcast.net>
2021-07-25 22:02:19 +0000

    sidekiq initializer 5

--- app/workers/unpaid_worker.rb
+++
@@ -1,8 +0,0 @@
-#app/workers/unpaid_worker.rb
-class UnpaidWorker
-  include Sidekiq::worker
-  def perform
-    unpaid_orders = Order.where('created_at < ? and paid = ?', 1.week.ago, true)
-    unpaid_orders.delete_all
-  end
-end
