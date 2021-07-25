ac484440bd37ff8aa067c95749aa7b696fd58431
wsuratt <wsuratt@comcast.net>
2021-07-25 02:53:33 +0000

    sidekiq initializer 2

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,8 +1,8 @@
 # config/initializers/sidekiq.rb
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://redis.example.com:7372/0' }
+  config.redis = { url: 'redis://redis.afillio.com:7372/0' }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://redis.example.com:7372/0' }
+  config.redis = { url: 'redis://redis.afillio.com:7372/0' }
 end
