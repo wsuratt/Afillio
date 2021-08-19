1fa0a0f051039079b09ecf844b9ecefd4991501c
wsuratt <wsuratt@comcast.net>
2021-08-19 22:09:00 +0000

    updated to stripe oauth live key

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -2,7 +2,7 @@ module OrdersHelper
   def stripe_button_link
     stripe_url = "https://connect.stripe.com/express/oauth/authorize"
     redirect_uri = stripe_connect_url
-    client_id = "ca_JewnrxC0ozaE2rNwNyJ7ot22tC7wYIXa"
+    client_id = Rails.application.credentials[:stripe][:oauth]
   
     "#{stripe_url}?redirect_uri=#{redirect_uri}&client_id=#{client_id}"
   end
