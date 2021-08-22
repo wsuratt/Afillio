2f2933890a9889e11df1b7d734709544ed428cc4
wsuratt <wsuratt@comcast.net>
2021-08-22 04:57:58 +0000

    only show active products on main index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,9 @@
 class HomeController < ApplicationController
   skip_before_action :authenticate_user!, :only => [:index, :how_it_works, :privacy_policy, :about_us, :become_vendor, :terms_conditions]
   def index
-    @latest = Product.latest
-    @popular = Product.popular
-    @sold_products = Product.joins(:orders).where(orders: {user: current_user, paid: true}).order(created_at: :desc).limit(4)
+    @latest = Product.where(show: true).latest
+    @popular = Product.where(show: true).popular
+    @sold_products = Product.where(show: true).joins(:orders).where(orders: {user: current_user, paid: true}).order(created_at: :desc).limit(4)
   end
 
   def privacy_policy
