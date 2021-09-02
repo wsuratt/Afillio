51b347b97ce599c68384031d9b81e5301848e756
wsuratt <wsuratt@comcast.net>
2021-09-02 02:16:56 +0000

    order mailer pt2

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -2,6 +2,6 @@ class OrderMailer < ApplicationMailer
     def new_order_email
     @order = params[:order]
 
-    mail(to: <@order.email>, subject: "Order confirmation")
+    mail(to: @order.email, subject: "Order confirmation")
   end
 end
