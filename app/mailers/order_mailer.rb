cd2be32b0ed3b11f4081761205c0284aaab353a2
wsuratt <wsuratt@comcast.net>
2021-09-07 03:05:05 +0000

    updated order mailer

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,7 +1,13 @@
 class OrderMailer < ApplicationMailer
-    def new_order_email
+  def new_order_email
     @order = params[:order]
 
     mail(to: @order.email, subject: "Order confirmation")
+  end
+  
+  def shipped_order_email
+    @order = params[:order]
+
+    mail(to: @order.email, subject: "Order shipped")
   end
 end
