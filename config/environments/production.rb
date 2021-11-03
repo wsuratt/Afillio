f652242ae9efd892eaf6599905e8907fc35ef7d1
wsuratt <wsuratt@comcast.net>
2021-11-03 02:30:52 +0000

    updated exception otification prefix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,7 +18,7 @@ Rails.application.configure do
   Rails.application.config.middleware.use ExceptionNotification::Rack,
   email: {
     deliver_with: :deliver, # Rails >= 4.2.1 do not need this option since it defaults to :deliver_now
-    email_prefix: '[PREFIX] ',
+    email_prefix: '[ERROR] ',
     sender_address: %{"Afillio error" <support@afillio.com>},
     exception_recipients: %w{williamsuratt1@gmail.com}
   }
