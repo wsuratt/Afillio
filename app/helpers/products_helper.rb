ddb534137881a20eed745ec0e783fa16f5236d00
wsuratt <wsuratt@comcast.net>
2021-12-16 06:43:59 +0000

    show product image carousel on new order

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 module ProductsHelper
+  def products_page?
+    current_page?(products_path) || current_page?(created_products_path) || \
+    current_page?(purchased_products_path) || current_page?(root_path)
+  end
 end
